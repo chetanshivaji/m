aac3ae759728fa949d0c13b1ab35bc4783e55e29
cmore <cmore@qualys.com>
2022-12-13 20:18:15 +0530

    package md

--- m.go
+++ m.go
@@ -3,12 +3,12 @@ package m
 import (
 	"fmt"
 
-	"github.com/chetanshivaji/m"
+	"github.com/chetanshivaji/md"
 	"github.com/chetanshivaji/n"
 )
 
 func Fun() {
 	fmt.Println("hello post 0.1.1")
 	n.Funn()
-	m.Funmd()
+	md.Funmd()
 }
