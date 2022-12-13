9765a7e3514b5005bc38db4986092016dcf52fdc
cmore <cmore@qualys.com>
2022-12-13 20:13:52 +0530

    added funmd

--- m.go
+++ m.go
@@ -3,10 +3,12 @@ package m
 import (
 	"fmt"
 
+	"github.com/chetanshivaji/m"
 	"github.com/chetanshivaji/n"
 )
 
 func Fun() {
 	fmt.Println("hello post 0.1.1")
 	n.Funn()
+	m.Funmd()
 }
---
+++ md.go
@@ -0,0 +1,7 @@
+package m
+
+import "fmt"
+
+func Funmd() {
+	fmt.Println("in fun md sub of m")
+}
