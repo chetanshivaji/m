c008821a7d13bad369c26ad5abb53629939623d8
cmore <cmore@qualys.com>
2022-12-13 19:55:24 +0530

    calling n from 0.1.2 from m

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/chetanshivaji/m
 
 go 1.19
+
+require github.com/chetanshivaji/n v0.1.2
--- m.go
+++ m.go
@@ -1,9 +1,12 @@
 package m
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/chetanshivaji/n"
+)
 
 func Fun() {
 	fmt.Println("hello post 0.1.1")
-
+	n.Funn()
 }
-
